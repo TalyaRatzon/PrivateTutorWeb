a5dd6feb2c3cbbf08d6b2bc4796a264ed4127063
ratzontalya <57313043+ratzontalya@users.noreply.github.com>
2020-09-21 14:17:05 +0300

    Let's get strarted

---
+++ BE/Section.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Section
+    {
+        public string Title { get; set; }
+        public string Data { get; set; }
+    }
+}
